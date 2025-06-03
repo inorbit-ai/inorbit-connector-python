673e2f2487bf06c7eadd78ca9d18b58ed74be9ea
Tomás Badenes <tomasbadenes@gmail.com>
2025-06-03 09:40:27 -0300

    Bump version: 1.1.2 → 1.1.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os
 
 from setuptools import find_packages, setup
 
-VERSION = "1.1.0"
+VERSION = "1.1.3"
 
 GITHUB_ORG_URL = "https://github.com/inorbit-ai"
 GITHUB_REPO_URL = f"{GITHUB_ORG_URL}/inorbit-connector-python"
